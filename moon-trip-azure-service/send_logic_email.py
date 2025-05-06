7bf905bbd2e35455a1b9f82effb0284c20b5f0dc
Similachi Andrei <sivfhd@yahoo.com>
2025-05-06 09:10:31 +0300

    fixed email sending

--- moon-trip-azure-service/send_logic_email.py
+++ moon-trip-azure-service/send_logic_email.py
@@ -6,9 +6,9 @@ def main():
     logic_app_url = 'https://prod-10.northcentralus.logic.azure.com:443/workflows/24da40b254f1492ab4664c5932f3d224/triggers/When_a_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_a_HTTP_request_is_received%2Frun&sv=1.0&sig=LvgDQBfH2PdYIK-SnBhZumuHiu2WIggjBUUlJwpYzT4'
 
     payload = {
-        "to": "sivfhd@yahoo.com",
+        "email": "tvandy600@gmail.com",
         "subject": "Test Email from Azure Function",
-        "body": "This email was triggered by a local script calling a Logic App."
+        "message": "This email was triggered by a local script calling a Logic App."
     }
 
     try:
